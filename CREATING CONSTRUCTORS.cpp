e0f4993c2a16d37919e993bacb7347b05e816171
Mosesmatisi <mosesmatisi@gmail.com>
2025-07-18 09:06:32 +0300

    Add files via upload

---
+++ CREATING CONSTRUCTORS.cpp
@@ -0,0 +1,15 @@
+///CREATING CONSTRUCTORS
+#include <iostream>
+using namespace std;
+class MyClass{
+    public:
+    MyClass(){
+        cout<<"Hello World!";
+        }
+};
+int main(){
+    MyClass myObj;
+    return 0;
+}
+
+    
